aeb11553fc8ff885e828d1ebd7d2f787752c3a28
jordanutz <jordanutz@outlook.com>
2019-01-11 20:41:46 -0700

    Completed filter strings

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -2,10 +2,38 @@ import React, {Component} from 'react'
 import '../../index.css'
 
 class FilterString extends Component {
+  constructor() {
+    super()
+    this.state = {
+      names: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy'],
+      filteredNames: [],
+      userInput: ''
+    }
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
+  filterNames = () => {
+    let filteredNames = this.state.names.filter( (name, index) => {
+      return name.includes(this.state.userInput)
+    })
+    this.setState({
+      filteredNames: filteredNames
+    })
+  }
+
   render () {
     return (
-      <div>
-        <h1>Filter String</h1>
+      <div className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText">{JSON.stringify(this.state.names)}</span>
+        <input className="inputLine" onChange={this.handleInput}/>
+        <button className="confirmationButton" onClick={this.filterNames}>Filter</button>
+        <span className="resultsBox filterStringRB">{JSON.stringify(this.state.filteredNames)}</span>
       </div>
     )
   }
